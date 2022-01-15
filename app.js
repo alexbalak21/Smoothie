cec20cac33005d3852c0283a23824bfcb8ae2655
alexbalak21 <alex.balak@outlook.com>
2022-01-16 00:00:31 +0100

    Working sign in, login, logout system

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/aouthRoutes");
 const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./controllers/middleware");
 
 const app = express();
 
@@ -24,6 +25,7 @@ mongoose
     .catch((err) => console.log(err));
 
 // routes
+app.get("*", checkUser);
 app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(authRoutes);
